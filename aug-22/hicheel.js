22bdfcd3ca2ac815ba98a8c9901952789e78acc1
Aniraa anir <aniraaanir@Aniraas-MacBook-Pro.local>
2024-08-22 13:46:40 -0400

    updated

---
+++ aug-22/hicheel.js
@@ -0,0 +1,85 @@
+/*
+const array = [
+  {
+    name: 'bodi',
+    age: 26,
+    siblings: [
+      {
+    name : 'bolor',
+    age: 15,
+      },
+      {
+    name: 'andy',
+    age: 22,
+      }
+    ]
+  }
+]
+console.log(array[0],siblings[1].name)
+*/
+
+
+
+const array= [
+  [
+    {
+      name: 'bill',
+    },
+    {
+      name:'dul',
+    },
+  ],
+  [
+    {
+      name:'alungoo',
+    },
+    {
+      name:'munkhuush',
+    },
+  ],
+];
+console.log(array[0][1].name);
+console.log(array[1][1].name);
+
+const array1 =[1,2,3,4,5]
+array1[0]=5
+array1[1]=4
+array1[2]=3
+array1[3]=2
+array1[4]=1
+console.log(array1)
+
+//reverse(1)
+const reverse = [1,2,3,4,5]
+reverse.reverse()
+console.log(reverse)
+//[5,4,3,2,1]
+ 
+//sort method 2
+const sort =[2,3,1,0,4]
+sort.sort()
+console.log(sort) 
+//[0,1,2,3,4]
+
+//tostring 3
+const tables=["banana","apple","orange"]
+console.log(tables.toString());
+// banana,apple,orange
+
+//includes 4
+const arrays =[100,200,300,400]
+console.log(arrays.includes(100))
+//100
+
+//push 5
+ const push=[1,2,3]
+ push.push(4)
+ console.log(push)
+ //1,2,3,4
+
+ //pop6
+ const pop=[1,2,3,4]
+ pop.pop()
+ console.log(pop)
+ //1.2.3
+ 
