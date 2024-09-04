20893e096d268000812d76731ce3abdcd06b53c2
Bodianir <andytur9@gmail.com>
2024-09-04 19:38:21 -0400

    bodlogo

--- bodlogo/bodlogo.js
+++ bodlogo/bodlogo.js
@@ -1,5 +1,5 @@
 // Define the array
-var numbers = [10, 20, 30, 40, 50];
+/*var numbers = [10, 20, 30, 40, 50];
 
 // Initialize a variable to store the sum
 var sum = 0;
@@ -74,3 +74,137 @@ for (let pow = 0; pow < 1000; pow++) {
     console.log(`ss ${ss} 1000 aas baga bn`);
   }
 }
+  */
+
+/*const arr = [1, 5, 3, 4, 10];
+let sum = 0;
+arr.map((element) => {
+  sum = sum + element;
+});
+console.log(sum);
+
+function doubleNumbers(array) {
+  const result = array.map((element) => {
+    return element * 2;
+  });
+  return result;
+  console.log(doubleNumbers([2, 5, 100]));
+}*/
+
+1;
+function stringItUp(arr) {
+  const result = arr.map((string) => string.toString());
+  return result;
+}
+
+console.log(stringItUp([2, 5, 100]));
+
+2;
+function capitalizeNames(arr) {
+  const capital = arr.map((cap) => cap.charAt(0).toUpperCase() + cap.slice());
+  return capital;
+}
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
+
+3;
+function namesOnly(arr) {
+  const cc = arr.map((string) => string.name);
+  return cc;
+}
+
+console.log(
+  namesOnly([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
+
+4;
+
+let c = 16;
+
+function makeStrings(arr) {
+  const results = arr.map((limit) => {
+    if (limit.age > c) {
+      return `${limit.name} can go to Matrix`;
+    } else {
+      return `${limit.name} is under age!!`;
+    }
+  });
+  results.forEach((result) => console.log(result));
+
+  return results;
+}
+
+console.log(
+  makeStrings([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
+5;
+function readyToPutInTheDOM(arr) {
+  // Use map to transform each object into an HTML string
+  return arr.map((item) => `<h1>${item.name}</h1><h2>${item.age}</h2>`);
+}
+
+console.log(
+  readyToPutInTheDOM([
+    {
+      name: "Angelina Jolie",
+      age: 80,
+    },
+    {
+      name: "Eric Jones",
+      age: 2,
+    },
+    {
+      name: "Paris Hilton",
+      age: 5,
+    },
+    {
+      name: "Kayne West",
+      age: 16,
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100,
+    },
+  ])
+);
